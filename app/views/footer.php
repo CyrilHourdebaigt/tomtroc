07c743deed0c8c36201bd6905bdd6839d4352823
Cyril <hourdebaigt_cyril@orange.fr>
2025-07-03 11:36:12 +0200

    Ajout de footer.php

---
+++ app/views/footer.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <div class="footer-content">
+        <p><a href="#">Politique de confidentialité</a></p>
+        <p><a href="#">Mentions légales</a></p>
+        <p>Tom Troc©</p>
+    </div>
+    <div class="footer-logo">
+        <img src="/tomtroc/public/assets/images/logo-tomtroc2.png" alt="TomTroc logo" />
+    </div>
+</footer>
